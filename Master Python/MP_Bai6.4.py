a28666aa4102eedad15f68d38663a70b55480a6f
WataruAki <25112007@vnu.edu.vn>
2026-04-26 02:36:59 +0700

    added Ex 6.4, 6.5, 6.6, 6.7

---
+++ Master Python/MP_Bai6.4.py
@@ -0,0 +1,4 @@
+text = ""
+line = input(text).split()
+word_count = len(line)
+print(word_count)
---
+++ Master Python/MP_Bai6.5.py
@@ -0,0 +1,10 @@
+n = int(input())
+nums = list(map(int, input().split()))
+even = 0
+odd = 0
+for num in nums:
+    if num % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print(f"Chẵn: {even}, Lẻ: {odd}" )
